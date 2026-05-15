5492465ca7758d6449f0336d77f6f36cafe28a0b
shohag-stack <shohag4y@gmail.com>
2026-05-15 12:26:31 +0600

    commentedout sendagent email

--- frontend/(core)/lib/sendAgentEmail.ts
+++ frontend/(core)/lib/sendAgentEmail.ts
@@ -1,29 +1,27 @@
-'use server'
+// 'use server'
 
-import { PropertyAgent } from "@/types"
-import sendEmail from "./sendEmail"
+// import sendEmail from "./sendEmail"
 
 
-type AgentForm = {
-  name: string
-  email: string
-  phone: string
-  message: string
-}
+// type AgentForm = {
+//   name: string
+//   email: string
+//   phone: string
+//   message: string
+// }
 
-export async function sendAgentEmail(form: AgentForm, agent: PropertyAgent, title: string) {
-  const [firstName, ...rest] = form.name.split(" ")
-  const lastName = rest.join(" ") || ""
+// export async function sendAgentEmail(form: AgentForm, title: string) {
+//   const [firstName, ...rest] = form.name.split(" ")
+//   const lastName = rest.join(" ") || ""
 
-  return await sendEmail({
-    name: form.name,
-    firstName: firstName,
-    lastName: lastName,
-    email: form.email,
-    phone: form.phone,
-    message: form.message,
-    property: title,
-    to: agent.email,
-    template: "agent"
-  })
-}
+//   return await sendEmail({
+//     name: form.name,
+//     firstName: firstName,
+//     lastName: lastName,
+//     email: form.email,
+//     phone: form.phone,
+//     message: form.message,
+//     property: title,
+//     to: agent.email,
+//   })
+// }
